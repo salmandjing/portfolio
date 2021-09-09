3a66053a5e94c2a3c6cb0fd3a574d0748e2c92f4
ITS Student <its-student@unl.edu>
2021-09-09 12:29:53 -0500

    a good start

---
+++ portfolio/src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import './App.css';
+import Header from './Components/Header';
+import About from './Components/About';
+import Resume from './Components/Resume';
+import Portfolio from './Components/Portfolio';
+import Testimonials from './Components/Testimonials';
+import Contact from './Components/Contact';
+import Footer from './Components/Footer';
+import {main,portfolio,resume,testimonials} from '../public/resumeData'
+
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      
+      resumeData:{
+        main: main,
+        portfolio: portfolio,
+        resume: resume
+      }
+    }
+  }
+
+
+
+
+  render() {
+    console.log(main);
+    return (
+      <div className="App">
+        <Header data={main} />
+        <About data={this.state.resumeData.main} />
+        <Resume data={this.state.resumeData.resume} />
+        <Portfolio data={this.state.resumeData.portfolio} />
+        <Contact data={this.state.resumeData.main} />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
